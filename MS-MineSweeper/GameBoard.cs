5633cb6ce8d40086499d48ba816bcb83183610ad
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2021-07-10 01:06:42 -0500

    Add project files.

---
+++ MS-MineSweeper/GameBoard.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static MineSweeperConsole.GameCell;
+
+
+/*
+ * This is the GameBoard class for the MineSweeper Game. In this class, there are 3 properties,
+ * the board size, the difficulty level, and a two-dimentional array that contains GameCell class objects.
+ */
+namespace MineSweeperConsole
+{
+    class GameBoard
+    {
+
+        /*
+         * The constructor initializes the two dimentional array with gameCell objects based on the size of the board
+         * given by the driver program.
+        */
+
+        public GameBoard(int boardSize)
+        {
+            this.BoardSize = boardSize;
+
+            grid = new GameCell[boardSize, boardSize];
+
+            for(int row = 0; row < boardSize; row++)
+            {
+                for(int col = 0; col < boardSize; col++)
+                {
+                    grid[row, col] = new GameCell(-1,-1,false,false,0);
+                }
+            }
+        }
+
+        //Properties
+        private int BoardSize { get; set; }
+        private int GameDifficulty { get; set; }
+        public GameCell[,] grid;
+
+
+        /*
+         * The SetupLiveMembers method is responsible for setting the live bombs on the gameboard. This is done by random
+         * numbers for the rows and columns in the two-dimentional array. To prevent from not placeing the proper number 
+         * of mines on the field, an if statment surrounded by a do/while will check the IsLive property of each randomly 
+         * selected GameCell in the array. If it is false, the mine will be planted and the loop ends. 
+         */
+        public void SetupLiveMembers(int difficulty)
+        {
+            int numMines = (BoardSize * BoardSize) / difficulty;
+
+            Random row = new Random();
+            Random col = new Random();
+                
+            for(int bombCount = 0; bombCount < numMines; bombCount++)
+            {
+                bool checker = false;
+                do
+                {
+                    int plantRow = row.Next(0, BoardSize);
+                    int plantCol = col.Next(0, BoardSize);
+
+                    if (grid[plantRow, plantCol].IsLive == false)
+                    {
+                        grid[plantRow, plantCol].IsLive = true;
+                        checker = true;
+                    }
+                } while (checker == false);
+            }
+
+        }
+        /*
+         * The purpose of the CalcLiveMembers method is to set the number of mines that neighbor each cell. This will be used
+         * in the displaying of the board.
+         */
+        public int CalcLiveMembers(int focusRow, int focusCol)
+        {
+            int liveMemberCounter = 0;
+
+            //if the cell has a mine on it
+            if(grid[focusRow, focusCol].IsLive)
+            {
+                liveMemberCounter = 9;
+            }
+            // case of top right corner
+            else if(focusRow == 0 && focusCol == 0)
+            {
+
+                if (grid[focusRow, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                
+            }
+            //case of botom right corner
+            else if(focusRow == BoardSize - 1 && focusCol == 0)
+            {
+                if (grid[focusRow - 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+            }
+            //case of bottom left corner
+            else if(focusRow == BoardSize - 1 && focusCol == BoardSize - 1)
+            {
+                if (grid[focusRow, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow  - 1, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+
+            }
+            //case of top left corner
+            else if (focusRow == 0 && focusCol == BoardSize - 1)
+            {
+                if (grid[focusRow, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+
+            }
+            // case Top Edge
+            else if(focusRow == 0 && focusCol != 0)
+            {
+                if(grid[focusRow, focusCol-1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                
+            }
+            // case left Edge
+            
+            else if (focusRow != 0 && focusCol == 0)
+            {
+                if (grid[focusRow - 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+
+            }
+            // case right Edge
+            else if (focusRow == 0 && focusCol != 0)
+            {
+                if (grid[focusRow - 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol -1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+
+            }
+            // case Bottom Edge
+            else if (focusRow == (BoardSize - 1) && focusCol != (BoardSize - 1))
+            {
+                if (grid[focusRow, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+
+            }
+            // case not corner or edge
+            else
+            {
+                if (grid[focusRow, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow - 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol + 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol - 1].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+                if (grid[focusRow + 1, focusCol].IsLive)
+                {
+                    liveMemberCounter++;
+                }
+            }
+
+            return liveMemberCounter;
+        }
+
+    }
+}
---
+++ MS-MineSweeper/Program.cs
@@ -0,0 +1,309 @@
+﻿using MineSweeperConsole;
+using System;
+
+namespace MS_MineSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************MineSweeper Game*****************\n");
+
+            //Sets the Board Size
+            int boardSize = 11;
+            GameBoard newBoard = new GameBoard(boardSize);
+
+            //Sets the Difficulty Level which depicts how many bombs are placed
+            newBoard.SetupLiveMembers(8);
+
+            //prints the initial Board
+            PrintBoard(newBoard, boardSize);
+
+            //GamePlay
+            PrintBoardDuringGame(newBoard, boardSize);
+
+        }
+
+        //This method drives the game play. It prints the board during the game based on the current
+        //cells that have been shown.
+        public static void PrintBoardDuringGame(GameBoard newBoard, int boardSize)
+        {
+
+            // declaration of game handling variables
+            int selectedRow, selectedCol;
+            bool playAgain = true;
+            bool gameOver = false;
+
+            // do/while loop for the game repition
+            do
+            {
+                // try/catch for exception handling
+                try
+                {
+
+                    // user input
+                    Console.Write("Enter the row: ");
+                    selectedRow = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the Column: ");
+                    selectedCol = int.Parse(Console.ReadLine());
+
+
+                    if (selectedRow > boardSize || selectedCol > boardSize)
+                    {
+                        Console.WriteLine("Your input was too large. try again");
+
+                    }
+                    else
+                    {
+                        newBoard.grid[selectedRow, selectedCol].ActiveState = true;
+                    }
+
+                    //Number index
+                    Console.Write("+ ");
+                    for (int boardBound = 0; boardBound < boardSize - 1; boardBound++)
+                    {
+                        if (boardBound < boardSize)
+                            Console.Write(boardBound + " + ");
+                        else
+                            Console.Write(boardBound + "+ ");
+                    }
+                    Console.WriteLine("");
+
+                    //top Boarder
+                    for (int boardBound = 0; boardBound < boardSize - 1; boardBound++)
+                    {
+
+                        Console.Write("+---");
+
+                        if (boardBound == boardSize - 1)
+                        {
+                            Console.WriteLine(boardBound + " +");
+                        }
+                    }
+                    Console.Write("+");
+
+                    //Grid
+                    for (int sendRow = 0; sendRow < boardSize - 1; sendRow++)
+                    {
+
+                        Console.Write(" \n: ");
+
+                        for (int sendCol = 0; sendCol < boardSize - 1; sendCol++)
+                        {
+
+
+                            if (sendCol < boardSize - 1)
+                            {
+                                int cell = newBoard.CalcLiveMembers(sendRow, sendCol);
+
+                                //checks the current entered row displays apprpriate cell
+                                if (selectedRow == sendRow && selectedCol == sendCol)
+                                {
+                                    if (cell == 9)
+                                    {
+                                        Console.Write("*", Console.ForegroundColor = ConsoleColor.Red);
+                                        Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                                        gameOver = true;
+                                    }
+                                    else
+                                    {
+                                        OutputSwitchCase(cell);
+                                    }
+
+                                }
+                                //prints the remainder of the board that is active state true
+                                else
+                                {
+                                    if (newBoard.grid[sendRow, sendCol].ActiveState == true)
+                                    {
+                                        OutputSwitchCase(cell);
+                                    }
+                                    else
+                                    {
+                                        Console.Write("?" + " : ");
+                                    }
+                                }
+                            }
+
+                        }
+
+                        //bottom row
+                        Console.Write("\n");
+                        for (int boardBound = 0; boardBound < boardSize - 1; boardBound++)
+                        {
+
+                            Console.Write("+---");
+
+                            if (boardBound == boardSize)
+                            {
+                                Console.WriteLine(boardBound + "+");
+                            }
+
+                        }
+                    }
+                    Console.WriteLine("+");
+                    Console.WriteLine("\n\n\n");
+                    playAgain = GameOverCheck(playAgain, gameOver);
+                    playAgain = CheckForWin(newBoard, boardSize, playAgain);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter a correct response");
+
+                }
+            } while (playAgain);
+
+        }
+
+        //This method controls the display of the live members and their colors. 
+        private static void OutputSwitchCase(int cell)
+        {
+            switch (cell)
+            {
+                case 0:
+                    Console.Write(" " + " : ");
+                    break;
+                case 1:
+                    Console.Write("1", Console.ForegroundColor = ConsoleColor.Green);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+                case 2:
+                    Console.Write("2", Console.ForegroundColor = ConsoleColor.Blue);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+                case 3:
+                    Console.Write("3", Console.ForegroundColor = ConsoleColor.Yellow);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+                case 4:
+                    Console.Write("4", Console.ForegroundColor = ConsoleColor.DarkCyan);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+                case 5:
+                    Console.Write("5", Console.ForegroundColor = ConsoleColor.Magenta);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+                case 6:
+                    Console.Write("6", Console.ForegroundColor = ConsoleColor.DarkYellow);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+                case 7:
+                    Console.Write("7", Console.ForegroundColor = ConsoleColor.DarkGreen);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+                case 8:
+                    Console.Write("8", Console.ForegroundColor = ConsoleColor.DarkRed);
+                    Console.Write(" : ", Console.ForegroundColor = ConsoleColor.White);
+                    break;
+            }
+        }
+
+        //This method checks if the the game is over.
+        private static bool GameOverCheck(bool playAgain, bool gameOver)
+        {
+            if (gameOver)
+            {
+                Console.WriteLine("\n\nYou Blowed Up!!!! Game Over");
+                playAgain = false;
+            }
+
+            return playAgain;
+        }
+
+        //This method checks to see if the game is won
+        private static bool CheckForWin(GameBoard newBoard, int boardSize, bool playAgain)
+        {
+            int winCount = 0;
+            for (int i = 0; i < boardSize - 1; i++)
+            {
+                for (int j = 0; j < boardSize - 1; j++)
+                {
+                    if (newBoard.grid[i, j].ActiveState == false && newBoard.grid[i, j].IsLive == true)
+                    {
+                        winCount++;
+                    }
+                }
+            }
+            if (winCount == 0)
+            {
+                playAgain = false;
+
+                Console.WriteLine("You have wone!!! And you didn't blow up!!!");
+            }
+
+            return playAgain;
+        }
+
+        // this method prints the initial board before gameplay
+        private static void PrintBoard(GameBoard newBoard, int boardSize)
+        {
+            int sideNums = 0;
+            //Top numbered index of board
+            Console.Write("+ ");
+            for (int boardBound = 0; boardBound < boardSize - 1; boardBound++)
+            {
+                if (boardBound < boardSize)
+                    Console.Write(boardBound + " + ");
+                else
+                    Console.Write(boardBound + "+ ");
+            }
+            Console.WriteLine("");
+
+            //Start of layout
+            for (int boardBound = 0; boardBound < boardSize - 1; boardBound++)
+            {
+
+                Console.Write("+---");
+
+                if (boardBound == boardSize - 1)
+                {
+                    Console.WriteLine(boardBound + " +");
+                }
+            }
+
+            //cells
+            for (int sendRow = 0; sendRow < boardSize - 1; sendRow++)
+            {
+
+                Console.Write(" \n: ");
+
+                for (int sendCol = 0; sendCol < boardSize - 1; sendCol++)
+                {
+                    int cell = newBoard.CalcLiveMembers(sendRow, sendCol);
+
+                    if (sendCol < boardSize - 1)
+                    {
+                        if (cell == 9)
+                            Console.Write("?" + " : ");
+                        else
+                            Console.Write("?" + " : ");
+
+                    }
+                    else if (sendCol == boardSize - 1)
+                    {
+                        if (cell == 9)
+                            Console.WriteLine("?" + " : " + sideNums + "\n");
+                        else
+                            Console.WriteLine("?" + " : " + sideNums + "\n");
+                        sideNums++;
+                    }
+                }
+                Console.Write("\n");
+                for (int boardBound = 0; boardBound < boardSize - 1; boardBound++)
+                {
+
+                    Console.Write("+---");
+
+                    if (boardBound == boardSize)
+                    {
+                        Console.WriteLine(boardBound + "+" + sideNums);
+                        sideNums++;
+                    }
+
+                }
+            }
+            Console.WriteLine("+");
+            Console.WriteLine("\n");
+        }
+    }
+}
